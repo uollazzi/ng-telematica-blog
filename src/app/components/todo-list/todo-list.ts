58e0828e048e66e9ddb867e2f4108750c1804525
Matteo Valenzi <dilloawallace@hotmail.com>
2026-01-21 09:50:32 +0100

    .

--- src/app/components/todo-list/todo-list.ts
+++ src/app/components/todo-list/todo-list.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../../services/todos';
 import { GetTodosResponse, Todo } from '../../models/todo';
 import { CommonModule } from '@angular/common';
@@ -11,15 +11,22 @@ import { CommonModule } from '@angular/common';
 })
 export class TodoList implements OnInit {
   todosService: TodosService = inject(TodosService);
-  todoResponse?: GetTodosResponse;
+
+  // definizione di signal
+  todoResponse = signal<GetTodosResponse | undefined>(undefined);
+
+  // computed signals
+  numeroTodoScaricati = computed(() => this.todoResponse()?.todos.length);
+  numeroTodoTotali = computed(() => this.todoResponse()?.total);
 
   ngOnInit(): void {
     this.todosService.getTodos()
       .subscribe(r => {
-        this.todoResponse = r;
-
+        // scrittura del valore del signal
+        this.todoResponse.set(r);
+        console.log(this.todoResponse());
         console.log("Todos scaricati con successo");
-        console.log(this.todoResponse.todos);
+
       });
   }
 }
